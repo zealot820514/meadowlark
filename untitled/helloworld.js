9d206d7e31dee475e0a9646e717d26616d2891ad
zhuyi <zealot820514@163.com>
2016-04-19 19:10:47 +0800

    all

--- untitled/helloworld.js
+++
@@ -1,7 +0,0 @@
-var http=require('http');
-http.createServer(function(req,res){
-	res.writeHead(200,{'content-type':'text/plain'});
-	res.end('hello world~');
-}).listen(3001);
-
-console.log('server start>>>>....ctrl-c terminate');
